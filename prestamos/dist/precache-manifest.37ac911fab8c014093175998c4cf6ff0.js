f8e137b959ac6eadc31301a0b63ced0c2d984009
Federico Cardozo <fedecar94@outlook.com>
2018-07-09 14:54:55 -0400

    dist

---
+++ prestamos/dist/precache-manifest.37ac911fab8c014093175998c4cf6ff0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a270aaadca493c99c748",
+    "url": "/js/chunk-vendors.a270aaad.js"
+  },
+  {
+    "revision": "af06aed37c3901a73743",
+    "url": "/js/app.af06aed3.js"
+  },
+  {
+    "revision": "b7296dc4fed47d9fff245b035c33719e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "af06aed37c3901a73743",
+    "url": "/css/app.710d7cae.css"
+  }
+];
